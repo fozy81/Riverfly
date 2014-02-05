fce4700b7bdf12617453d02dc9aaca15a8f7341e
fozy81 <fozy81@hotmail.com>
2014-02-05 22:15:13 +0000

    redirecting shiny server to new data source

--- server.R
+++ server.R
@@ -1,8 +1,34 @@
 library(shiny)
 library(ggplot2)
+library(RCurl)
+library(reshape)
 
+myCsv <- getURL("https://docs.google.com/spreadsheet/pub?key=0ArVD_Gwut6UBdHZkQ2g0U0NXQ0psZUltQkpKZjVEM3c&output=csv")
+o2 <- read.csv(textConnection(myCsv))
 
-load("dataClean.RData")
+o2$dateClean  <- strptime(o2$Survey.date, "%d/%m/%Y")
+o2$id <- sequence(nrow(o2))
+
+o3 <- melt(o2, id.vars=c("id","dateClean","Site.", "Survey.date", "CC0","Comments","Timestamp"))
+o3$value <- as.numeric(o3$value)
+o3$dateClean2 <- as.character(o3$dateClean)
+o3$site <- as.character(o3$Site.)
+o3$Site. <- NULL
+o3$dateClean <- NULL
+
+o3$log[o3$value < 10] <- 1
+o3$log[o3$value >= 10 & o3$value < 100] <- 2
+o3$log[o3$value >= 100 & o3$value < 100000] <- 3
+
+dt.o3 <- data.table(o3, key=c("dateClean2","site"))
+
+total <- dt.o3[,list(Total=sum(log),
+                     trigger=2
+), by=key(dt.o3)]
+
+dataClean <- data.frame(total)
+dataClean$date  <- strptime(dataClean$dateClean2, "%Y-%m-%d")
+
 
 # Define server logic required to summarize and view the selected dataset
 shinyServer(function(input, output) {
@@ -10,13 +36,14 @@ shinyServer(function(input, output) {
   # Return the requested dataset
     
     formulaText <- reactive({
-      summaryData <- eval(parse(text=paste("dataClean[dataClean$site == \"", input$dataset, "\",1:5]",sep="")))
-      summaryData$date <- as.character(summaryData$date)
+      summaryData <- eval(parse(text=paste("o3[o3$site == \"", input$dataset, "\"& o3$value != 0, 6:9]",sep="")))
+      summaryData$date <- as.character(summaryData$dateClean2)
+      summaryData$dateClean2 <- NULL
       return(summaryData)
       })
             
       tableText <- reactive({
-        eval(parse(text=paste("dataClean[dataClean$site == \"", input$dataset, "\" & dataClean$name == \"Total\", 1:6]",sep="")))    
+        eval(parse(text=paste("dataClean[dataClean$site == \"", input$dataset, "\", 2:5]",sep="")))    
       })
         
         captionText <- reactive({
@@ -36,29 +63,29 @@ shinyServer(function(input, output) {
   )
     
     
-  # Show the first "n" observations
+  # Show "Total" riverfly score on graph
   output$view <- renderPlot({
     dataset <- tableText()
 
-    if (length(dataset$value) >= 2){
-     print(ggplot(data=dataset, aes(x=date, y=value, fill=value,colour="Score")) + 
+    if (length(dataset$Total) >= 2){
+     print(ggplot(data=dataset, aes(x=date, y=Total, fill=Total, colour="value")) + 
              geom_bar(stat="identity") + 
-             geom_text(aes(x=date, y=value, label=date, 
-             vjust=ifelse(sign(value)>0, 2, 0)),
+             geom_text(aes(x=date, y=Total, label=date, 
+             vjust=ifelse(sign(Total)>0, 2, 0)),
                        position = position_dodge(width=1)) + 
              geom_abline(data=dataClean, aes(colour="Trigger Level",intercept=trigger,slope=0,size=2)) +
             scale_colour_manual(name = 'Trigger',values=c("Trigger Level"="red","value"="grey")) +
              ylab("Riverfly Score") + xlab("Date"))
     }
     else {
-      if(length(dataset$value) < 2){
+      if(length(dataset$Total) < 2){
       dataset <- rbind(dataset,dataset[1,])
-      dataset$date[2] <- as.Date('2013-12-30') 
-      dataset$value[2] <- 0
-          print(ggplot(data=dataset, aes(x=date, y=value, fill=value,colour="value")) + 
+      dataset$date[2] <- strptime('2013-12-30', "%Y-%m-%d")
+      dataset$Total[2] <- 0
+          print(ggplot(data=dataset, aes(x=date, y=Total, fill=Total,colour="value")) + 
                   geom_bar(stat="identity") + 
-                  geom_text(aes(x=date, y=value, label=date, 
-                  vjust=ifelse(sign(value)>0, 2, 0)),
+                  geom_text(aes(x=date, y=Total, label=date, 
+                  vjust=ifelse(sign(Total)>0, 2, 0)),
       position = position_dodge(width=1)) + 
                       geom_abline(aes(colour="Trigger Level",intercept=trigger,slope=0,size=2)) + 
                 scale_colour_manual(name = 'Trigger',values=c("Trigger Level"="red","value"="grey")) +
--- ui.R
+++ ui.R
@@ -1,8 +1,31 @@
 library(shiny)
 library(ggplot2)
+library(RCurl)
+library(reshape)
+myCsv <- getURL("https://docs.google.com/spreadsheet/pub?key=0ArVD_Gwut6UBdHZkQ2g0U0NXQ0psZUltQkpKZjVEM3c&output=csv")
+o2 <- read.csv(textConnection(myCsv))
 
+o2$dateClean  <- strptime(o2$Survey.date, "%d/%m/%Y")
+o2$id <- sequence(nrow(o2))
 
-load("dataClean.RData")
+o3 <- melt(o2, id.vars=c("id","dateClean","Site.", "Survey.date", "CC0","Comments","Timestamp"))
+o3$value <- as.numeric(o3$value)
+o3$dateClean2 <- as.character(o3$dateClean)
+o3$site <- as.character(o3$Site.)
+o3$Site. <- NULL
+o3$dateClean <- NULL
+
+o3$log[o3$value < 10] <- 1
+o3$log[o3$value >= 10 & o3$value < 100] <- 2
+o3$log[o3$value >= 100 & o3$value < 100000] <- 3
+
+dt.o3 <- data.table(o3, key=c("dateClean2","site"))
+
+total <- dt.o3[,list(Total=sum(log)             
+), by=key(dt.o3)]
+
+dataClean <- data.frame(total)
+dataClean$date  <- as.Date(dataClean$dateClean2, "%Y-%m-%d")
 
 # Define UI for dataset viewer application
 shinyUI(pageWithSidebar(
