085b0f5827322e8dcdb9ac5418c1b8322dd8d105
fozy81 <fozy81@hotmail.com>
2014-05-04 11:21:40 +0100

    improved date format in table

--- server.R
+++ server.R
@@ -63,9 +63,9 @@ d2$value <- as.numeric(d2$value)
 
 # Data for 'All sites' tab
 # combine d and dataClean for full data with trigger & riverfly score values for new tab containing all data in one
-  dataFull <- cbind(d,dataClean)  
+  dataFull <- cbind(dataClean,d)  
 # create data.frame (table) only with nice readable names for displaying
-  dataFull <- dataFull[, c("Site" ,  "Survey date"  ,"Mayfly" , "Stonefly","Freshwater shrimp", "Flat bodied (Heptageniidae)", "Cased caddis", "Caseless Caddis" ,"Olives (Baetidae)", "Blue Winged Olives (Ephemerellidae)","Comments", "Combined Riverfly Score" , "Default Trigger Level")]  
+  dataFull <- dataFull[, c("Site" ,  "Survey Date"  ,"Mayfly" , "Stonefly","Freshwater shrimp", "Flat bodied (Heptageniidae)", "Cased caddis", "Caseless Caddis" ,"Olives (Baetidae)", "Blue Winged Olives (Ephemerellidae)","Comments", "Combined Riverfly Score" , "Default Trigger Level")]  
 # URL of google spreadsheet with site information
   myCsv2 <- getURL("https://docs.google.com/spreadsheet/pub?key=0ArVD_Gwut6UBdHZkQ2g0U0NXQ0psZUltQkpKZjVEM3c&single=true&gid=1&output=csv") # get site details from google doc (list of all sites - even ones without sample results)
 
@@ -85,10 +85,8 @@ dat <- sites[,c('lat', 'long', 'Full.name')]
 # for values for table - reactive depending on which site is selected  
     formulaText <- reactive({
       summaryData <- eval(parse(text=paste("dataFull[dataFull$Site == \"", input$dataset, "\", 2:12]",sep="")))
-      summaryData$'Survey date' <- strptime(summaryData$'Survey date', "%d/%m/%y")    
-      summaryData$'Survey date' <- format(summaryData$'Survey date', "%y/%m/%d")
-     
-      summaryData <-  summaryData[ order( summaryData$'Survey date', decreasing = TRUE),]
+       
+      summaryData <-  summaryData[ order( summaryData$'Survey Date', decreasing = TRUE),]
             return(summaryData)
       })
 # values for graph plotting - reactive depending on which site is selected           
