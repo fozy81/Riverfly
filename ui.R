f200acd07585eae0d6f9eefc64feff2afb8ee75f
fozy81 <fozy81@hotmail.com>
2014-09-06 16:35:25 +0100

    add url.js

--- ui.R
+++ ui.R
@@ -38,7 +38,7 @@ shinyUI(pageWithSidebar(
     ),
   
   # Show a summary of the dataset and plot
-  mainPanel(  
+  mainPanel(    includeHTML("URL.js"),
     tabsetPanel(
       tabPanel("Site Results", h3(textOutput("caption")), plotOutput("view"), h4("Site Summary"),tableOutput("siteStats"),  h4("Site Data"),dataTableOutput("summary")),
       tabPanel("All Results", h4("Summary"), tableOutput("stats"),plotOutput("histogram"),plotOutput("cumsum"),h4("Duplicates in data?"),tableOutput("dupes"),h4("All Data"),dataTableOutput("allresults"))
